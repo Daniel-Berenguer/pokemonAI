040753f6566b5306d15c572f8ff90ed3accf4ba5
Daniel Berenguer <danielbmasia@gmail.com>
2025-09-03 19:36:48 +0200

    data1

--- src/downloadGames.py
+++ src/downloadGames.py
@@ -9,7 +9,7 @@ existing_games = os.listdir("data/games/")
 
 new = 0
 
-for p in range(1,35):
+for p in range(1,30):
     print("Page number: ", p)
     options = Options()
     options.add_argument("--headless=new") 
